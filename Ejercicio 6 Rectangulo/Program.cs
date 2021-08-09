065ebd52928aca16ad71e21015a95b8077929ace
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:27:00 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio 6 Rectangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_6_Rectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo(10, 10);
+        }
+        public static void Rectangulo(int altura, int anchura)
+        {
+            for (int i = 0; i <= (altura - 1); i++)
+            {
+                for (int f = 0; f <= (anchura - 1); f++)
+                {
+                    Console.Write("***");
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
